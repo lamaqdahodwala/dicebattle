bbd1fcb4518e9b5b9ad5e1846735c1687ebf76a8
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-06-10 10:39:53 -0400

    created javascript things

---
+++ static/main.js
@@ -0,0 +1,17 @@
+let total = 0
+let r1 = 0
+let r2 = 0
+let r3 = 0
+let r4 = 0
+let r5 = 0
+
+
+
+function roll(){
+    let x = Math.ceil(Math.random() * 6)
+    if (x == 0){
+        return 1
+    } 
+    return x
+}
+
